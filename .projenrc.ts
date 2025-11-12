48c30e770d06636daebaf8de1b92764ca9ad4b32
Gary Sassano <10464497+garysassano@users.noreply.github.com>
2025-11-12 23:27:57 +0100

    chore: project created with projen

---
+++ .projenrc.ts
@@ -0,0 +1,15 @@
+import { awscdk, javascript } from "projen";
+const project = new awscdk.AwsCdkTypeScriptApp({
+  biome: true,
+  cdkVersion: "2.1.0",
+  defaultReleaseBranch: "main",
+  name: "cdk-aws-ecs-managed-instance",
+  packageManager: javascript.NodePackageManager.PNPM,
+  projenrcTs: true,
+
+  // deps: [],                /* Runtime dependencies of this module. */
+  // description: undefined,  /* The description is just a string that helps people understand the purpose of the package. */
+  // devDeps: [],             /* Build dependencies for this module. */
+  // packageName: undefined,  /* The "name" in package.json. */
+});
+project.synth();
